09725d39dc24d6973312dd2e427f1d6584304a0b
kishanup78910 <kishanup789@gmail.com>
2024-07-07 01:14:17 +0530

    ckjdfjdfkj

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express =require("express")
 const app =express();
 
+app.get("/",(req,res)=>{
+    res.send("hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh")
+})
 
 app.listen(3000,()=>{
     console.log("server is up running at 3000")
