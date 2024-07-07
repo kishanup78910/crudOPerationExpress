b1b821d9620619f9518593c90a3070bf85e2a45f
kishanup78910 <kishanup789@gmail.com>
2024-07-08 00:29:59 +0530

    jmodiles

--- index.js
+++ index.js
@@ -1,9 +1,48 @@
 const express =require("express")
 const app =express();
 
+
+let book=[
+    {
+      "id": 1,
+      "author": {
+        "name": "Jane Doe",
+        "email": "jane.doe@example.com"
+      }
+    },
+    {
+      "id": 2,
+      "author": {
+        "name": "John Smith",
+        "email": "john.smith@example.com"
+      }
+    },
+    {
+      "id": 3,
+      "author": {
+        "name": "Alice Johnson",
+        "email": "alice.johnson@example.com"
+      }
+    }
+  ];
+  
+app.use(express.json)  
+
 app.get("/",(req,res)=>{
-    res.send("hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh")
+    res.json(book)
 })
+//update
+app.post('/books',(req,res)=>{
+    console.log(req.body);
+    const newbook = req.body;
+    newbook.id = book.lengthj+4;
+    book.push(newbook);
+    res.status(201).json(newbook);
+    
+})
+
+
+//update
 
 app.listen(3000,()=>{
     console.log("server is up running at 3000")
